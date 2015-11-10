3d92780bd1d95047a75a5cd2b48b37d5994e250a
oriyentel <jrocnc@mail.missouri.edu>
2015-11-09 20:47:17 -0600

    commit to attempt to fix database error in openshift

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,32 +1,46 @@
 <?php
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: Josh
+ * Date: 11/2/15
+ * Time: 11:14 PM
+ */
 
-class Login extends CI_Controller{
-    
-        function __construct(){
-                parent::__construct();
-        }
-    
-        public function index(){
-                // Load login view
-                $this->load->view('login_view');
-        }
-    
-        public function process(){
-                // Load the model
-                $this->load->model('login_model');
-                // Validate login info
-                $result = $this->login_model->validate();
-                // Now we verify the result
-                if(! $result){
-                        //Incorrect login info, redirect back to login page
-                        redirect('login');
-                }else{
-                        //Correct login info, redirect home page
-                        redirect('home');
-                }        
-        }
+    // We need to start a session
+    session_start();
+
+class Login extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/login
+     *	- or -
+     * 		http://example.com/login/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+
+    public function __construct() {
+
+        // construct the base object
+        parent::__construct();
+
+        // Load helpers and libraries (we have already loaded these using 'autoload.php')
+        // $this->load->helper('form');
+        // $this->load->library('form_validation');
+        // $this->load->library('session');
+
+        // Load the database
+
+
+    }
+
 }
-
-?>
