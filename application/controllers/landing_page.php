a8305695ed2ba7a3968aaa33771b9dbcd26b06f0
Emeka Okoye <Emeka@mwc-024190.dhcp.missouri.edu>
2015-11-11 20:36:20 -0600

    Created landing page model and changed controller/view

--- application/controllers/landing_page.php
+++ application/controllers/landing_page.php
@@ -8,12 +8,19 @@
  */
 class landing_page extends CI_Controller {
 
+
     public function __construct() {
         parent::__construct();
+        $this->load->model('landing_model');
     }
 
     public function index() {
-        $this->load->view('landing_page_view');
+
+        $empID = $this->session->userdata('empID');
+        $listData = $this->landing_model->view_admin($empID);
+        $data = array("listData" => $listData);
+        $this->load->view('landing_page_view', $data);
+
     }
 
 }
