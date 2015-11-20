c38484d0782dc4c5f869a8020d72170f8903f80f
David <davidecasper@gmail.com>
2015-11-20 17:58:23 -0600

    database seed data

--- application/database/sisrequest_sprint3.sql
+++ application/database/sisrequest_sprint3.sql
@@ -19,7 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `sisrequest`
 --
-
+CREATE DATABASE IF NOT EXISTS sisrequest;
+USE sisrequest;
 -- --------------------------------------------------------
 
 --
