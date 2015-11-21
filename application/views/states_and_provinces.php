d4d7af696946af48cf1e019da65d711c6a6c31dc
oriyentel <joshuaosteen@me.com>
2015-11-20 23:27:53 -0600

    added styling for all access requests

--- application/views/states_and_provinces.php
+++ application/views/states_and_provinces.php
@@ -6,7 +6,7 @@
  * Time: 1:41 PM
  */
 ?>
-    <select id="stateSelection">
+    <select id="stateSelection" class="form-control">
         <option value="Alabama">Alabama</option>
         <option value="Alaska">Alaska</option>
         <option value="Arizona">Arizona</option>
